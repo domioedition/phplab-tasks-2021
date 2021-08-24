8c4d2a41a5e8ff2d6bdc05141694e16192eb2b58
aCooler <asformyself@gmail.com>
2021-08-24 00:36:42 +0000

    [arrays] Add one file

---
+++ src/arrays/Arrays.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace arrays;
+
+class Arrays implements ArraysInterface
+{
+    /**
+     * @param array $input
+     * @return array
+     */
+    public function repeatArrayValues(array $input): array
+    {
+        $result = [];
+        foreach ($input as $element) {
+            for ($i = 0; $i < $element; $i++) {
+                array_push($result, $element);
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param array $input
+     * @return int
+     */
+    public function getUniqueValue(array $input): int
+    {
+        $counter = 0;
+        $uniqueDigits = [];
+        foreach ($input as $digit) {
+            isset($uniqueDigits[$digit]) ? $uniqueDigits[$digit] = false :
+            $uniqueDigits[$digit] = true;
+        }
+
+
+        foreach ($uniqueDigits as $key => $value) {
+            if ($value) {
+                if ($counter == 0 || $counter > $key) {
+                    $counter = $key;
+                }
+            }
+        }
+        return $counter;
+    }
+
+    /**
+     * @param array $input
+     * @return array
+     */
+    public function groupByTag(array $input): array
+    {
+        $result= [];
+        foreach ($input as $element) {
+            $name = $element["name"];
+            $tags = $element["tags"];
+            foreach ($tags as $tag) {
+                if (!isset($result[$tag])) {
+                    $result[$tag] = [];
+                }
+                if (!in_array($name, $result[$tag])) {
+                    array_push($result[$tag], $name);
+                }
+            }
+        }
+
+        foreach ($result as  $key => $value) {
+            sort($value);
+            $result[$key] = $value;
+        }
+
+        return $result;
+    }
+}
