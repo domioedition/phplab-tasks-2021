ce5578434624b1df563e7a05ad82b5f744e24c8e
aCooler <asformyself@gmail.com>
2021-08-24 17:13:57 +0000

    [strings] Refactored and updated branch from master

--- src/strings/Strings.php
+++ src/strings/Strings.php
@@ -1,56 +1,52 @@
 <?php
+
 namespace strings;
 
-class Strings implements StringsInterface {
+class Strings implements StringsInterface
+{
     /**
      * @param string $input
      * @return string
      */
-    public function snakeCaseToCamelCase(string $input):
-        string {
-
-            $camelCaseString = "";
-            for ($i = 0;$i < strlen($input);$i++) {
-                if ($input[$i] == "_") {
-                    $i++;
-                    $camelCaseString .= strtoupper($input[$i]);
-                }
-                else {
-                    $camelCaseString .= $input[$i];
-                }
-
+    public function snakeCaseToCamelCase(string $input): string
+    {
+        $camelCaseString = "";
+        for ($i = 0;$i < strlen($input);$i++) {
+            if ($input[$i] == "_") {
+                $i++;
+                $camelCaseString .= strtoupper($input[$i]);
+            } else {
+                $camelCaseString .= $input[$i];
             }
-            return $camelCaseString;
-
         }
+        return $camelCaseString;
+    }
 
-        /**
-         * @param string $input
-         * @return string
-         */
-        public function mirrorMultibyteString(string $input):
-            string {
-                $multiString = explode(" ", $input);
-                $result = [];
-                foreach ($multiString as $word) {
-                    preg_match_all('/./us', $word, $box);
-                    $word = implode('', array_reverse($box[0]));
-                    array_push($result, $word);
-                }
-                return implode(" ", $result);
-            }
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function mirrorMultibyteString(string $input): string
+    {
+        $multiString = explode(" ", $input);
+        $result = [];
+        foreach ($multiString as $word) {
+            preg_match_all('/./us', $word, $box);
+            $word = implode('', array_reverse($box[0]));
+            array_push($result, $word);
+        }
+        return implode(" ", $result);
+    }
 
-            /**
-             * @param string $noun
-             * @return string
-             */
-            public function getBrandName(string $noun):
-                string {
-                    if ($noun[0] == $noun[strlen($noun) - 1]) {
-                        return ucfirst($noun) . substr($noun, 1);
-                    }
-                    return "The " . ucfirst($noun);
-                }
-
-            }
-?>
+    /**
+     * @param string $noun
+     * @return string
+     */
+    public function getBrandName(string $noun): string
+    {
+        if ($noun[0] == $noun[strlen($noun) - 1]) {
+            return ucfirst($noun) . substr($noun, 1);
+        }
+        return "The " . ucfirst($noun);
+    }
+}
